8bbfe36c03b2302942339e0e96dff8b933ced4d8
erchiggins <emilyrchiggins@gmail.com>
2018-04-12 16:07:32 -0400

    more structure

--- meetup-dino-service/src/main/java/com/example/controller/DinoController.java
+++ meetup-dino-service/src/main/java/com/example/controller/DinoController.java
@@ -1,5 +1,49 @@
 package com.example.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.model.Dinosaur;
+import com.example.model.Message;
+import com.example.service.DinoService;
+
+@RestController
+@CrossOrigin
 public class DinoController {
+	
+	@Autowired 
+	private DinoService ds;
+	
+	@GetMapping("/all")
+	public ResponseEntity<List<Dinosaur>> getAllDinosaures() {
+		return new ResponseEntity<>(ds.getAllDinos(), HttpStatus.OK);
+	}
+	
+	@PostMapping("/addDino")
+	public ResponseEntity<Message> addDinosaur(@RequestBody Dinosaur dino) {
+
+		ResponseEntity<Message> resp = null;
+		try {
+			ds.addDino(dino);
+			resp = new ResponseEntity<>(new Message("DINO CREATED SUCCESSFULLY"), HttpStatus.OK);
+		} catch (Exception e) {
+			resp = new ResponseEntity<>(new Message("FAILED TO CREATE DINO"), HttpStatus.BAD_REQUEST);
+		}
+		return resp;
+
+	}
+	
+	@GetMapping("/test")
+	public String getZuulTestMessage(){
+		return "hello from T-REX SERVICE!";
+	}
 
 }
--- meetup-dino-service/src/main/java/com/example/model/Dinosaur.java
+++ meetup-dino-service/src/main/java/com/example/model/Dinosaur.java
@@ -14,44 +14,53 @@ import org.springframework.stereotype.Component;
 @Component("dinosaur")
 @Scope("prototype")
 @Entity
-@Table(name="DINOSAUR")
+@Table(name = "DINOSAUR")
 public class Dinosaur {
-	
+
 	public Dinosaur(String name, String species) {
 		super();
 		this.name = name;
 		this.species = species;
 	}
+
 	public Dinosaur() {
 		super();
 	}
+
 	@Id
-	@GeneratedValue(strategy=GenerationType.SEQUENCE,generator="dinoSequence")
-	@SequenceGenerator(allocationSize=1,name="dinoSequence",sequenceName="SQ_DINO_PK")
-	@Column(name="DINO_ID")
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "dinoSequence")
+	@SequenceGenerator(allocationSize = 1, name = "dinoSequence", sequenceName = "SQ_DINO_PK")
+	@Column(name = "DINO_ID")
 	private int id;
-	@Column(name="DINO_NAME")
+	@Column(name = "DINO_NAME")
 	private String name;
-	@Column(name="DINO_SPECIES")
+	@Column(name = "DINO_SPECIES")
 	private String species;
+
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public String getSpecies() {
 		return species;
 	}
+
 	public void setSpecies(String species) {
 		this.species = species;
 	}
+
 	@Override
 	public String toString() {
 		return "Dinosaur [id=" + id + ", name=" + name + ", species=" + species + "]";
